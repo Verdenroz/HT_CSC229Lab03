1a102a85d68070376ce1330dbcf43be3cc46cd0d
Harvey Tseng <117050810+Verdenroz@users.noreply.github.com>
2023-02-13 22:50:36 -0500

    Update CSC229Lab03_TimeComplexity.java

--- CSC229Lab03_TimeComplexity.java
+++ CSC229Lab03_TimeComplexity.java
@@ -3,26 +3,44 @@
 
 public class CSC229Lab03_TimeComplexity {
     public static void main(String[] args) {
-        
+        int[] arr = {1, 3, 5, 8, 9};
+        System.out.println(getLargest(arr, arr.length));
     }
 
 
     //What is the time complexity of this function?
     public static int getLargest(int arr[], int sz){
-        int iterate1 = 0;
-        int iterate2 = 0;
-        int largest = 0;
-        while (iterate < sz - 1){
-            iterate2++;
-            if (iterate2 == sz){
-                iterate1++;
+        int iterate1 = 0;       // 1 step
+        int iterate2 = 0;       // 1 step
+        int largest = 0;        // 1 step
+        while (iterate1 < sz - 1){
+            iterate2++;         //n steps
+                                                     //System.out.println(iterate1);              trace iterate1
+                                                     //System.out.println(iterate2);              trace iterate2
+           
+            if (iterate2 == sz){ //n steps
+                iterate1++;                 
                 iterate2 = iterate1;
+                                                      //System.out.println("Hit");         trace if block
                 continue;
             }
-            int product = arr[iterate1] * arr[iterate2];
+            int product = arr[iterate1] * arr[iterate2];    //n steps
             if (product > largest)
                 largest = product;
         }
-        return largest;
+        return largest;     // 1 step
+
+        /*
+         * Consider arr = {1, 3, 5, 8, 9} and sz = 5
+         * The function works by matching each element to every other element
+         * Essentially like two nested for loops
+         * 
+         * The while loop is n steps
+         * The if block inside the while loop is n steps
+         * 
+         * As such: n * n + 1 + 1 + 1 + 1 =  O(n^2)     Quadratic time complexity
+         * 
+         */
+
     }
 }
